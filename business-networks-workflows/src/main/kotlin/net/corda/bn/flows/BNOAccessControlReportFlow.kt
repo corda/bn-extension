970e1d0085658c6fdcd48cb09799129ccd5c28cd
nikinagy <nikolett.nagy@r3.com>
2020-09-07 09:45:34 +0100

    address PR comments

--- business-networks-workflows/src/main/kotlin/net/corda/bn/flows/BNOAccessControlReportFlow.kt
+++ business-networks-workflows/src/main/kotlin/net/corda/bn/flows/BNOAccessControlReportFlow.kt
@@ -1,87 +1,69 @@
 package net.corda.bn.flows
 
 import co.paralleluniverse.fibers.Suspendable
-import net.corda.bn.contracts.MembershipContract
 import net.corda.bn.states.BNRole
 import net.corda.bn.states.GroupState
+import net.corda.bn.states.MembershipIdentity
 import net.corda.bn.states.MembershipStatus
 import net.corda.core.contracts.StateAndRef
-import net.corda.core.contracts.UniqueIdentifier
-import net.corda.core.flows.FinalityFlow
-import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.InitiatingFlow
 import net.corda.core.flows.StartableByRPC
-import net.corda.core.flows.ReceiveFinalityFlow
-import net.corda.core.flows.InitiatedBy
-import net.corda.core.flows.FlowSession
-import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 import net.corda.core.serialization.CordaSerializable
-import net.corda.core.transactions.TransactionBuilder
 import java.io.File
+import java.time.Instant
 
 @CordaSerializable
-data class AccessControlReport(val members: MutableMap<AbstractParty, MemberInfos>)
+data class AccessControlReport(val members: MutableMap<MembershipIdentity, MemberInfos>, val groups: MutableSet<GroupInfos>)
 
 @CordaSerializable
 data class MemberInfos(val membershipStatus: MembershipStatus, val groups: MutableSet<String> = mutableSetOf(), val roles: Set<BNRole>)
 
+@CordaSerializable
+data class GroupInfos(val name: String?, val participants: List<Party>)
+
 @InitiatingFlow
 @StartableByRPC
 class BNOAccessControlReportFlow(
-        private val networkId: UniqueIdentifier = UniqueIdentifier(),
-        private val writeToFile: Boolean = false,
-        private val notary: Party? = null
-) : FlowLogic<AccessControlReport>() {
+        private val networkId: String,
+        private val writeToFile: Boolean = false
+) : MembershipManagementFlow<AccessControlReport>() {
 
     @Suspendable
-    @Suppress("NestedBlockDepth")
     override fun call(): AccessControlReport {
         val bnService = serviceHub.cordaService(BNService::class.java)
-        val ourMembership = bnService.getMembership(networkId.toString(), ourIdentity)
-                ?: throw MembershipNotFoundException("$ourIdentity is not member of Business Network with $networkId ID")
 
-        validate(bnService)
+        authorise(networkId, bnService) { it.canActivateMembership() && it.canModifyBusinessIdentity() && it.canModifyGroups() && it.canModifyMembership() && it.canModifyRoles() && it.canRevokeMembership() && it.canSuspendMembership() }
 
         //list all members in the network with their membership status and roles
         val allMembersOnTheNetwork = getAllMembersWithRolesAndStatus(bnService)
 
         //collect all groups and their members
         val allGroupsOnTheNetwork = getAllBusinessNetworkGroups(bnService)
+        val groupInfos: MutableSet<GroupInfos> = mutableSetOf()
         allGroupsOnTheNetwork.forEach {
-            collectTheGroups(it, allMembersOnTheNetwork)
+            collectTheGroupsForMembers(it, allMembersOnTheNetwork)
+            groupInfos.add(GroupInfos(it.state.data.name, it.state.data.participants))
         }
 
-        val reports = AccessControlReport(allMembersOnTheNetwork)
+        val reports = AccessControlReport(allMembersOnTheNetwork, groupInfos)
 
         if(writeToFile) {
             writeToFile(reports)
         }
 
-        //log the report
-
-        val builder = TransactionBuilder(notary ?: serviceHub.networkMapCache.notaryIdentities.first())
-                .addInputState(ourMembership)
-                .addCommand(MembershipContract.Commands.AccessControlReport(), ourIdentity.owningKey)
-
-        val stx = serviceHub.signInitialTransaction(builder)
-        val sessions = (allMembersOnTheNetwork.keys - ourIdentity).map { initiateFlow(it) }
-        subFlow(FinalityFlow(stx, sessions))
+        logReports(reports, logger::info) {
+            logger.info("")
+        }
 
         return reports
     }
 
-    private fun validate(bnService: BNService) {
-        if(!bnService.businessNetworkExists(networkId.toString())) {
-            throw BusinessNetworkNotFoundException("Business Network with $networkId doesn't exist")
-        }
-    }
+    private fun getAllMembersWithRolesAndStatus(bnService: BNService): MutableMap<MembershipIdentity, MemberInfos> {
+        val allMembers = mutableMapOf<MembershipIdentity, MemberInfos>()
 
-    private fun getAllMembersWithRolesAndStatus(bnService: BNService): MutableMap<AbstractParty, MemberInfos> {
-        val allMembers = mutableMapOf<AbstractParty, MemberInfos>()
-
-        bnService.getAllMemberships(networkId.toString()).forEach { it ->
-            allMembers.put(it.state.data.identity.cordaIdentity,
+        bnService.getAllMemberships(networkId).forEach { it ->
+            allMembers.put(it.state.data.identity,
                                 MemberInfos(membershipStatus = it.state.data.status,
                                 roles = it.state.data.roles))
         }
@@ -89,48 +71,61 @@ class BNOAccessControlReportFlow(
     }
 
     private fun getAllBusinessNetworkGroups(bnService: BNService): List<StateAndRef<GroupState>> {
-        return bnService.getAllBusinessNetworkGroups(networkId.toString())
+        return bnService.getAllBusinessNetworkGroups(networkId)
     }
 
-    private fun collectTheGroups(groupState: StateAndRef<GroupState>, allMembersOnTheNetwork: MutableMap<AbstractParty, MemberInfos>) {
+    private fun collectTheGroupsForMembers(groupState: StateAndRef<GroupState>, allMembersOnTheNetwork: MutableMap<MembershipIdentity, MemberInfos>) {
         val groupName = groupState.state.data.name
         if(groupName != null){
-            groupState.state.data.participants.forEach {
-                if(allMembersOnTheNetwork.containsKey(it)) {
-                    allMembersOnTheNetwork.get(it)!!.groups.add(groupName)
+            allMembersOnTheNetwork.forEach { (key, value) ->
+                if(groupState.state.data.participants.contains(key.cordaIdentity)) {
+                    value.groups.add(groupName)
                 }
             }
         }
     }
 
     private fun writeToFile(reports: AccessControlReport) {
+        val currentTime = Instant.now()
         //find a suitable place for the file
-        val fileName = "myfile.txt"
+        val fileName = "bno-access-control-report-$currentTime.txt"
         val reportFile = File(fileName)
 
         reportFile.printWriter().use { out ->
-            out.println("Access Control report for network $networkId\n")
-
-            reports.members.forEach { party, infos ->
-                out.println("Party name: $party")
-                out.println("Membership status: ${infos.membershipStatus}")
-                out.println("Groups: ${infos.groups}")
-                out.println("Roles:")
-                infos.roles.forEach {
-                    out.println("Name of the role: ${it.name}")
-                    out.println("Permissions: ${it.permissions}")
-                }
-                out.println()
-            }
+            logReports(reports, out::println, out::println)
         }
     }
-}
 
-@InitiatedBy(BNOAccessControlReportFlow::class)
-class BNOAccessControlReportObserverFlow(private val session: FlowSession) : FlowLogic<Unit>() {
+    private fun logReports(reports: AccessControlReport, loggerFunction: (String) -> Unit, newLineFunction: () -> Unit) {
+        loggerFunction("Access Control report for network $networkId")
+        newLineFunction()
 
-    @Suspendable
-    override fun call() {
-        subFlow(ReceiveFinalityFlow(session))
+        loggerFunction("Participants on the network: ")
+        newLineFunction()
+        reports.members.forEach { party, infos ->
+            loggerFunction("Party identity:")
+            loggerFunction("Corda identity: ${party.cordaIdentity}")
+            loggerFunction("Business idenity: ${party.businessIdentity}")
+            loggerFunction("Membership status: ${infos.membershipStatus}")
+            loggerFunction("Groups: ${infos.groups}")
+            loggerFunction("Roles:")
+            if(infos.roles.isEmpty()) {
+                loggerFunction("There are no roles associated with this identity.")
+            } else {
+                infos.roles.forEach {
+                    loggerFunction("Name of the role: ${it.name}")
+                    loggerFunction("Permissions: ${it.permissions}")
+                }
+            }
+            newLineFunction()
+        }
+
+        loggerFunction("Groups on the network: ")
+        newLineFunction()
+        reports.groups.forEach {
+            loggerFunction("Name of the group: ${it.name}")
+            loggerFunction("Participants: ${it.participants}")
+            newLineFunction()
+        }
     }
 }
